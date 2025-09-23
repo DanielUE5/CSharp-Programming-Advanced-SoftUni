b88144c6ee771a56167a2f29cfafc4eb59c25259
Daniel <dani94@mail.bg>
2025-09-23 16:34:46 +0300

    Add files via upload

---
+++ Stacks & Queues/Traffic Jam/Traffic Jam/Program.cs
@@ -0,0 +1,29 @@
+﻿int passedCars = int.Parse(Console.ReadLine());
+string input  = Console.ReadLine();
+
+Queue<string> trafficJam = new Queue<string>(passedCars);
+
+int allPassedCars = 0;
+
+while (input != "end")
+{
+    if (input != "green")
+    {
+        trafficJam.Enqueue(input);
+    }
+    else if (input == "green")
+    {
+        int currentPass = passedCars;
+        while (trafficJam.Count > 0 && currentPass > 0)
+        {
+            Console.WriteLine($"{trafficJam.Dequeue()} passed!  ");
+            currentPass--;
+            allPassedCars++;
+        }
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"{allPassedCars} cars passed the crossroads.");
+
