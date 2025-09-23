29d3678f632b15140827e5dc7e3aa9a111b4ffcc
Daniel <dani94@mail.bg>
2025-09-23 16:32:45 +0300

    Add files via upload

---
+++ Stacks & Queues/Simple Calculator/Simple Calculator/SimpleCalc.cs
@@ -0,0 +1,35 @@
+﻿class SimpleCalc
+{
+    static void Main(string[] args)
+    {
+        string expression = Console.ReadLine();
+
+        string[] tokens = expression.Split(' ');
+
+        Stack<string> stack = new Stack<string>();
+        for (int i = tokens.Length - 1; i >= 0; i--)
+        {
+            stack.Push(tokens[i]);
+        }
+
+        int result = int.Parse(stack.Pop());
+
+        while (stack.Count > 0)
+        {
+            string operatorSymbol = stack.Pop();
+
+            int number = int.Parse(stack.Pop());
+
+            if (operatorSymbol == "+")
+            {
+                result += number;
+            }
+            else if (operatorSymbol == "-")
+            {
+                result -= number;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Stacks & Queues/Stack Sum/Stack Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] input = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> numbers = new Stack<int>(input);
+
+
+while (true)
+{
+    string[] commands = Console.ReadLine().Split();
+    string command = commands[0].ToLower();
+
+    if (command == "end")
+        break;
+
+    if (command == "add")
+    {
+        int num1 = int.Parse(commands[1]);
+        int num2 = int.Parse(commands[2]);
+
+        numbers.Push(num1);
+        numbers.Push(num2);
+    }
+    else if (command == "remove")
+    {
+        int count = int.Parse(commands[1]);
+
+        if (count <= numbers.Count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                {
+                    numbers.Pop();
+                }
+            }
+        }
+    }
+}
+Console.WriteLine($"Sum: {numbers.Sum()}");
