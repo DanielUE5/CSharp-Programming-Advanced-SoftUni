bf0814c1611c3dbd0566185b5df33fd9068a5e90
Daniel <dani94@mail.bg>
2025-10-25 03:42:27 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Parking Lot/Parking Lot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+internal class ParkingLot
+{
+    private static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+
+        HashSet<string> set = new HashSet<string>();
+
+        while (input != "END")
+        {
+
+            string[] inputParams = Regex.Split(input, ", ");
+
+            if (inputParams[0] == "IN")
+            {
+                set.Add(inputParams[1]);
+            }
+            else
+            {
+                set.Remove(inputParams[1]);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        if (set.Count == 0)
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+        else
+        {
+            foreach (var car in set)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/SoftUni Party/SoftUni Party/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class SoftUniParty
+{
+    public static void Main()
+    {
+        HashSet<string> vipGuests = new HashSet<string>();
+        HashSet<string> regularGuests = new HashSet<string>();
+
+        var input = Console.ReadLine();
+
+        while (input != "PARTY")
+        {
+            if (char.IsDigit(input[0])) 
+            {
+                vipGuests.Add(input);
+            }
+            else
+            {
+                regularGuests.Add(input);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        input = Console.ReadLine();
+        while (input != "END")
+        {
+            if (vipGuests.Contains(input))
+            {
+                vipGuests.Remove(input);
+            }
+            else if (regularGuests.Contains(input))
+            {
+                regularGuests.Remove(input); 
+            }
+
+            input = Console.ReadLine();
+        }
+
+        int guestsNotArrived = vipGuests.Count + regularGuests.Count;
+        Console.WriteLine(guestsNotArrived);
+
+        foreach (var vip in vipGuests)
+        {
+            Console.WriteLine(vip);
+        }
+
+        foreach (var regular in regularGuests)
+        {
+            Console.WriteLine(regular);
+        }
+    }
+}
