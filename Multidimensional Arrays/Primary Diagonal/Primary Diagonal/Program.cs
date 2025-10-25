ce469d2105b4516e5003457c0cfc1479f8b5fcbf
Daniel <dani94@mail.bg>
2025-10-25 03:34:23 +0300

    Add files via upload

---
+++ Multidimensional Arrays/Primary Diagonal/Primary Diagonal/Program.cs
@@ -0,0 +1,25 @@
+﻿int squareMatrix = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[squareMatrix, squareMatrix];
+
+for (int i = 0; i < squareMatrix; i++)
+{
+    int[] data = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+    for (int j = 0; j < squareMatrix; j++)
+    {
+        matrix[i, j] = data[j];
+    }
+}
+
+int sum = 0;
+
+for (int i = 0; i < squareMatrix; i++)
+{
+    sum += matrix[i, i];
+}
+
+Console.WriteLine(sum);
---
+++ Multidimensional Arrays/Sum Matrix Elements/Sum Matrix Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] dimensions = Console.ReadLine()
+    .Split(", ")
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = dimensions[0], cols = dimensions[1];
+int sum = 0;
+
+int[,] matrix = ReadMatrix(rows, cols);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum += matrix[i, j];
+    }
+}
+
+Console.WriteLine(matrix.GetLength(0));
+Console.WriteLine(matrix.GetLength(1));
+Console.WriteLine(sum);
+
+
+
+
+static int[,] ReadMatrix(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int[] data = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = data[j];
+        }
+    }
+    return matrix;
+}
