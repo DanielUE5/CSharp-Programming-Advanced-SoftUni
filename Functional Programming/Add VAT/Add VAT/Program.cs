2545cda964bee4e411c5637b86c533e2f62be9b3
Daniel <dani94@mail.bg>
2025-10-26 00:32:53 +0300

    Add files via upload

---
+++ Functional Programming/Add VAT/Add VAT/Program.cs
@@ -0,0 +1,21 @@
+﻿class addVAT
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        double[] prices = input.Split(", ")
+                               .Select(double.Parse)
+                               .ToArray();
+
+        const double VAT = 0.20;
+
+        var pricesWithVAT = prices.Select(p => p * (1 + VAT))
+                                  .Select(p => p.ToString("F2"));
+
+        foreach (var price in pricesWithVAT)
+        {
+            Console.WriteLine(price);
+        }
+    }
+}
