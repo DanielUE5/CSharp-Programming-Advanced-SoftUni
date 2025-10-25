13bd3938a580b23ef7f48e08ad24bd45f8cbaf88
Daniel <dani94@mail.bg>
2025-10-25 03:41:57 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Record Unique Names/Record Unique Names/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class RecordUniqueNames
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        HashSet<string> set = new HashSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            set.Add(Console.ReadLine());
+        }
+
+        foreach (var name in set)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
