5481fc736c0f25c3f5b2ccfed4d1b5cc24429588
Daniel <dani94@mail.bg>
2025-10-26 00:43:27 +0300

    Add files via upload

---
+++ Functional Programming/Predicate for Names/Predicate for Names/Program.cs
@@ -0,0 +1,23 @@
+﻿class PredicateForNames
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<string> names = Console.ReadLine().Split(' ').ToList();
+
+        PrintFilteredNames(names, n);
+    }
+
+    static void PrintFilteredNames(List<string> names, int maxLength)
+    {
+        Func<string, bool> filterByLength = name => name.Length <= maxLength;
+
+        List<string> filteredNames = names.Where(filterByLength).ToList();
+
+        foreach (var name in filteredNames)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
