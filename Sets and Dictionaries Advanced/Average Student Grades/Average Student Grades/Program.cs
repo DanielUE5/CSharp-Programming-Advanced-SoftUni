cb3f272a79802986a774de6a4b2757fd126ec780
Daniel <dani94@mail.bg>
2025-10-25 03:41:08 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Average Student Grades/Average Student Grades/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class AverageGrades
+{
+    public static void Main(string[] args)
+    {
+        int studentsCount = int.Parse(Console.ReadLine());
+        Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+        for (int i = 0; i < studentsCount; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            string studentName = input[0];
+            decimal grade = decimal.Parse(input[1]);
+
+            if (!students.ContainsKey(studentName))
+            {
+                students.Add(studentName, new List<decimal>());
+            }
+            students[studentName].Add(grade);
+        }
+
+        foreach (var (student, grades) in students)
+        {
+            string[] mappedGrades = grades.Select(grade => $"{grade:f2}").ToArray();
+
+            Console.WriteLine($"{student} -> {string.Join(" ", mappedGrades)} (avg: {grades.Average():F2})");
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/Count Same Values in Array/Count Same Values in Array/Program.cs
@@ -0,0 +1,17 @@
+﻿List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+Dictionary<double, int> numberTimes = new Dictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if (!numberTimes.ContainsKey(number))
+    {
+        numberTimes.Add(number, 0);
+    }
+
+    numberTimes[number]++;
+}
+
+foreach (var numberTime in numberTimes)
+{
+    Console.WriteLine($"{numberTime.Key} - {numberTime.Value} times");
+}
