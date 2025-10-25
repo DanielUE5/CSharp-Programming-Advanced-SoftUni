57e3d6dc0cba25606d133ed4189afd4f035e77f1
Daniel <dani94@mail.bg>
2025-10-25 03:37:26 +0300

    Add files via upload

---
+++ Multidimensional Arrays/Exercises/Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,28 @@
+﻿int squareMatrix = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[squareMatrix, squareMatrix];
+
+for (int i = 0; i < squareMatrix; i++)
+{
+    int[] data = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+    for (int j = 0; j < squareMatrix; j++)
+    {
+        matrix[i, j] = data[j];
+    }
+}
+
+int sum1 = 0;
+int sum2 = 0;
+
+for (int i = 0; i < squareMatrix; i++)
+{
+    sum1 += matrix[i, i];
+    sum2 += matrix[i, squareMatrix - i - 1];
+}
+
+
+Console.WriteLine(Math.Abs(sum1-sum2));
---
+++ Multidimensional Arrays/Exercises/Maximal Sum/Maximal Sum/Program.cs
@@ -0,0 +1,65 @@
+﻿public class Program
+{
+    const int M = 3, N = 3;
+
+    public static void Main()
+    {
+        int[] dimensions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        int rows = dimensions[0], cols = dimensions[1];
+
+        int[,] matrix = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            int[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = data[j];
+            }
+        }
+
+        int maxSum = int.MinValue, maxOriginRow = -1, maxOriginCol = -1;
+
+        for (int i = 0; i <= rows - M; i++)
+        {
+            for (int j = 0; j <= cols - N; j++)
+            { 
+
+                int currentSum = SumSubMatrix(matrix, i, j);
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    maxOriginRow = i;
+                    maxOriginCol = j;
+                }
+            }
+        }
+        Console.WriteLine($"Sum = {maxSum}");
+        PrintSubMatrix(matrix, maxOriginRow, maxOriginCol);
+    }
+
+    private static int SumSubMatrix(int[,] matrix, int row, int col)
+    {
+        int sum = 0;
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++) 
+                sum += matrix[row + i, col + j];
+        }
+        return sum;
+    }
+
+    private static void PrintSubMatrix(int[,] matrix, int row, int col)
+    {
+        for(int i = 0;i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (j > 0) Console.Write(' ');
+                Console.Write(matrix[row + i, col + j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
