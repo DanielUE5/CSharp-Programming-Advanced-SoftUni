2d867f467864f0aa6d1e36fc333005242069e565
Daniel <dani94@mail.bg>
2025-10-26 00:42:39 +0300

    Add files via upload

---
+++ Functional Programming/Exercises/Find Evens or Odds/Find Evens or Odds/Program.cs
@@ -0,0 +1,37 @@
+﻿class FindEvensOrOdds
+{
+    static void Main()
+    {
+        int[] bounds = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int lowerBound = bounds[0];
+        int upperBound = bounds[1];
+
+        string command = Console.ReadLine();
+
+        Predicate<int> filter;
+
+        if (command == "even")
+        {
+            filter = number => number % 2 == 0;  
+        }
+        else if (command == "odd")
+        {
+            filter = number => number % 2 != 0;  
+        }
+        else
+        {
+            throw new ArgumentException("Invalid command");
+        }
+
+        List<int> result = new List<int>();
+        for (int i = lowerBound; i <= upperBound; i++)
+        {
+            if (filter(i))
+            {
+                result.Add(i);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
---
+++ Functional Programming/Exercises/Reverse and Exclude/Reverse and Exclude/Program.cs
@@ -0,0 +1,17 @@
+﻿class ReverseAndExclude
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        int n = int.Parse(Console.ReadLine());
+
+        Predicate<int> isDivisibleByN = number => number % n == 0;
+
+        numbers.Reverse();
+
+        List<int> result = numbers.Where(num => !isDivisibleByN(num)).ToList();
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
