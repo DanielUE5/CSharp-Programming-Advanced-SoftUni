2fa6e36165c651c7806e6b04734fd8ca19124362
Daniel <dani94@mail.bg>
2025-10-25 03:35:12 +0300

    Add files via upload

---
+++ Multidimensional Arrays/Jagged-Array Modification/Jagged-Array Modification/Program.cs
@@ -0,0 +1,34 @@
+﻿int rows = int.Parse(Console.ReadLine());
+
+int[][] matrix = new int[rows][];
+for (int i = 0; i < rows; i++)
+{
+    matrix[i] = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
+}
+string command = Console.ReadLine();
+
+while (command != "END")
+{
+
+    string[] data = command.Split();
+    string operation = data[0];
+    int row = int.Parse(data[1]), 
+        col = int.Parse(data[2]), 
+        val = int.Parse(data[3]);
+
+    if (row < 0 || row > matrix.Length || col < 0 || col >= matrix[row].Length)
+        Console.WriteLine("Invalid coordinates");
+    else if (operation == "Add")
+        matrix[row][col] += val;
+    else if (operation == "Subtract")
+        matrix[row][col] -= val;
+
+    command = Console.ReadLine();
+
+}
+
+foreach (int[] row in matrix)
+    Console.WriteLine(string.Join(' ', row));
---
+++ Multidimensional Arrays/Symbol in Matrix/Symbol in Matrix/Program.cs
@@ -0,0 +1,40 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+char[,] matrix = new char[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    char[] data = Console.ReadLine().ToCharArray();
+    
+
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = data[j];
+    }
+}
+
+char symbol = Console.ReadLine()[0];
+bool found = false;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (matrix[i, j] == symbol)
+        {
+            Console.WriteLine($"({i}, {j})");
+            found = true;
+            break; 
+        }
+    }
+    if (found)
+    {
+        break;
+    }
+}
+
+if (!found)
+{
+    Console.WriteLine($"{symbol} does not occur in the matrix");
+}
+    
