67b7ea35f7ce0a64a65a9b7033e5b6ffdca53224
Daniel <dani94@mail.bg>
2026-01-10 17:49:24 +0200

    Add files via upload

---
+++ Generics/Box of T/Box of T/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxofT
+{
+    public class Box<T>
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            T element = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            return element;
+        }
+
+        public int Count => elements.Count;
+    }
+}
---
+++ Generics/GenericScale/GenericScale/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            EqualityScale<int> scale1 = new EqualityScale<int>(5, 5);
+            Console.WriteLine(scale1.AreEqual()); // Output: True
+
+            EqualityScale<int> scale2 = new EqualityScale<int>(3, 5);
+            Console.WriteLine(scale2.AreEqual()); // Output: False
+        }
+    }
+}
+
